d063acce675ca8de2a412c334a5d7d76e4a54966
bhanuchwodary <bhanuprabhakar.b@gmail.com>
2025-03-15 13:39:56 +0400

    android app

---
+++ android/app/src/main/java/com/lovable/streamify/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lovable.streamify;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,23 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.streamify.jukebox',
-  appName: 'Streamify Jukebox',
-  webDir: 'dist',
-  server: {
-    url: 'https://ef95c806-4d6e-446d-9829-24f650a152ff.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  android: {
-    buildOptions: {
-      keystorePath: undefined,
-      keystoreAlias: undefined,
-      keystorePassword: undefined,
-      keystoreAliasPassword: undefined,
-      signingType: 'apksigner',
-    },
-  }
+  appId: 'com.lovable.streamify',
+  appName: 'streamify',
+  webDir: 'dist'
 };
 
 export default config;
